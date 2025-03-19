c88c27963d7bdad8f7e2b981b26cb9001e4ef04f
Pc_Huy <pchuy4003@gmai.com>
2025-03-19 23:14:46 +0700

    swag

--- cmd/swag/docs/docs.go
+++ cmd/swag/docs/docs.go
@@ -2994,7 +2994,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0.0",
-	Host:             "localhost:8080",
+	Host:             "yourvibes.duckdns.org:8080",
 	BasePath:         "/v1/2024",
 	Schemes:          []string{},
 	Title:            "API Documentation YourVibes backend",
